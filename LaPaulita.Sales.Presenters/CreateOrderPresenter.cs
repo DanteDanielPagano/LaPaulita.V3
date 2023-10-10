1da21f88bc49cb24cee8ceb5641c93d5f97e9243
DanteDanielPagano <dpagano@mendoza.edu.ar>
2023-10-10 16:54:44 -0300

    .

--- LaPaulita.Sales.Presenters/CreateOrderPresenter.cs
+++ LaPaulita.Sales.Presenters/CreateOrderPresenter.cs
@@ -4,24 +4,16 @@ namespace LaPaulita.Sales.Presenters
 {
     public class CreateOrderPresenter : ICreateOrderPresenter
     {
-        //public int OrderId { get; private set; }
-
-        //public List<ValidationErrorDto> ErrorsList { get; private set; }
 
         public WrappersSalesOrder Order { get; private set; } = new WrappersSalesOrder();
 
-        //public ValueTask Handle(int orderId)
-        //{
-        //    OrderId = orderId;
-        //    return ValueTask.CompletedTask;
-        //}
 
         public ValueTask Handle(WrappersSalesOrder order)
         {
             Order.Errors = order.Errors;
             Order.Message = order.Message;
             Order.OrderId = order.OrderId;
-            Order.Errors = order.Errors;
+            Order.ErrorNumber = order.ErrorNumber;
             return ValueTask.CompletedTask;
         }
 
